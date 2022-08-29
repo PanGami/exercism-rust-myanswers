9b0c422ffa9810ad81b11b6d07aea7fb21a33965
Pan <pancanugrahawicaksana@gmail.com>
2022-08-29 14:18:33 +0700

    Done

--- sublist/src/lib.rs
+++ sublist/src/lib.rs
@@ -7,5 +7,15 @@ pub enum Comparison {
 }
 
 pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
-    unimplemented!("Determine if the first list is equal to, sublist of, superlist of or unequal to the second list.");
+    use self::Comparison::*;
+    match (_first_list.len(), _second_list.len()) {
+        (0, 0) => Equal,
+        (0, _) => Sublist,
+        (_, 0) => Superlist,
+        (first_len, second_len) if first_len < second_len => 
+            if _second_list.windows(first_len).any(|sl| sl == _first_list) { Sublist } else { Unequal },
+        (first_len, second_len) if first_len > second_len => 
+            if _first_list.windows(second_len).any(|sl| sl == _second_list) { Superlist } else { Unequal },
+        (_, _) => if _first_list == _second_list { Equal } else { Unequal }
+    }
 }
